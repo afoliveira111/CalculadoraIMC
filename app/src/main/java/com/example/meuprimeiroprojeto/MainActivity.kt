e0779f0146ae115bb5eaf8796070cf7e701d0500
Felipe <felipetermometria@hotmail.com>
2023-10-27 14:52:26 -0300

    update, toolbar

--- app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val btnCalcular: Button = findViewById(R.id.btnCalcular)
         val edtPeso : EditText = findViewById(R.id.edittext_peso)
@@ -24,18 +29,14 @@ class MainActivity : AppCompatActivity() {
             val pesoStr = edtPeso.text.toString()
 
             if(alturaStr.isNotEmpty() && pesoStr.isNotEmpty()){
-
-
             val altura : Float = alturaStr.toFloat()
             val peso: Float = pesoStr.toFloat()
-
             val alturaFinal: Float = altura * altura
             val result: Float = peso / alturaFinal
 
             val intent = Intent(this, ResultActivity::class.java)
                 .apply {
                     putExtra("EXTRA_RESULT", result)
-
                 }
                 startActivity(intent)
                  }else{
@@ -43,8 +44,8 @@ class MainActivity : AppCompatActivity() {
 
                }
 
-            }
+          }
 
-        }
+     }
 
-    }
+}
